480fa8d3746e5f6df77b9ccc8af42d81231f9237
Pierre <pierre@example.local>
2025-01-17 11:21:20 +0100

    save

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+const gravity = 0.2
+
 canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0,0,canvas.width, canvas.height)
+c.fillStyle = 'black'
 
 class Sprite{
  
     constructor({position, velocity}){
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
 
     draw(){
@@ -19,8 +23,20 @@ class Sprite{
     }
     update(){
         this.draw()
+        this.velocity.y += gravity
         this.position.y += this.velocity.y
+        if (this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0
+        }
     }
+}
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.clearRect(0,0,canvas.width,canvas.height)
+    player.update()
+    ennemy.update()
 }
 
 const player = new Sprite({
@@ -41,12 +57,5 @@ player.draw()
 ennemy.draw()
 console.log(player)
 
-function animate(){
-    window.requestAnimationFrame(animate)
-    c.fillStyle = 'black'
-    c.clearRect(0,0,canvas.width,canvas.height)
-    player.update()
-    ennemy.update()
-}
 
 animate()
